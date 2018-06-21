f1be2f614a4efbd9a62b2d8c02592e7a90e7f88f
Tomash <tomash@MacBook-Pro-Andrii.local>
2018-06-21 20:43:48 +0300

    fix

--- server/ssr/preloadDate.js
+++ server/ssr/preloadDate.js
@@ -1,11 +1,10 @@
-import url from 'url';
 import { matchRoutes } from 'react-router-config'
 import { routes } from '../../src/routes'
 
 export const PreloadDateInit = (req, store) => {
   const branch = matchRoutes(routes, req.path)
     const promises = branch.map(({ route, match }) => {
-      if (match.url === url.parse(req.url).pathname && route.PreloadDate) {
+      if (route.PreloadDate) {
           return Promise.all(
             route
               .PreloadDate(match.params)
